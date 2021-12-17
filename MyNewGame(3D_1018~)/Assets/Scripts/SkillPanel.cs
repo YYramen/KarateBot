14063ab6d2561ebd5416eef3ef1f3ac7e0a6343a
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2021-12-17 18:03:55 +0900

    スキルのUIを作ろうとしている

---
+++ MyNewGame(3D_1018~)/Assets/Scripts/SkillPanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillPanel : MonoBehaviour
+{
+    [SerializeField] GameObject _skillPanel;
+    [Header ("Skill1")]
+    [SerializeField] Text _skillText;
+    [SerializeField] string _skillString = "スキル1";
+    [Space(10)]
+    [Header("Skill2")]
+    [SerializeField] Text _skillText2;
+    [SerializeField] string _skillString2 = "スキル２";
+    [Space(10)]
+    [SerializeField] Text _skillText3;
+    [SerializeField] string _skillString3 = "スキル３";
+    
+
+    // Update is called once per frame
+    void Start()
+    {
+        _skillText.text = _skillString;
+        _skillText2.text = _skillString2;
+        _skillText3.text = _skillString3;
+    }
+}
