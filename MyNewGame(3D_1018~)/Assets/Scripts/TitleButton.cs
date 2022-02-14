1d7bf21daad7859b09a763ef36f31fa8195ca39b
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-02-14 18:31:49 +0900

    タイトルシーン完成

---
+++ MyNewGame(3D_1018~)/Assets/Scripts/TitleButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// ボタンに使うスクリプト。主にシーンの移動
+/// </summary>
+
+public class TitleButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.google.co.jp");
+#else
+		Application.Quit();
+#endif
+    }
+}
